aa88348684b1b20c1e0c7106bf0cc2c1787ea715
peasizedsunflower <mtlabiola@gmail.com>
2021-04-13 13:22:49 -0600

    testing

--- js/main.js
+++ js/main.js
@@ -47,33 +47,33 @@
 					setTimeout(function(){
 						$span.addClass('appear')
 					}, i*250);
-					// this.destroy();
+					this.destroy();
 				},
-				// offset: '95%'
+				offset: '95%'
 			});
 		});
 	});
 
 	// Galery Owl
-	$('#galery-owl').owlCarousel({
-		items:1,
-		loop:true,
-		margin:0,
-		dots : false,
-		nav: true,
-		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		autoplay : true,
-		autoplaySpeed :500,
-		navSpeed :500,
-		responsive : {
-	    0 : {
-	       stagePadding : 0,
-	    },
-	    768 : {
-	        stagePadding : 120,
-	    }
-		}
-	});
+	// $('#galery-owl').owlCarousel({
+	// 	items:1,
+	// 	loop:true,
+	// 	margin:0,
+	// 	dots : false,
+	// 	nav: true,
+	// 	navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+	// 	autoplay : true,
+	// 	autoplaySpeed :500,
+	// 	navSpeed :500,
+	// 	responsive : {
+	//     0 : {
+	//        stagePadding : 0,
+	//     },
+	//     768 : {
+	//         stagePadding : 120,
+	//     }
+	// 	}
+	// });
 
 	// Parallax Background
 	$.stellar({
