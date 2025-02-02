cf293c8980c1f565a7423b375ae43a5ed8d8b6f4
user <user@CYBER.local>
2025-02-02 09:36:36 +0100

    vite update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  server: {
+    historyApiFallback: true, // This ensures React Router handles all routes
+  },
+});
