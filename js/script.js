b0773b1303dd9d53f36ccb13975d36000c0e423d
HaymosBobby <haymosx@gmail.com>
2025-03-04 19:13:42 +0100

    crypto udpate

---
+++ js/script.js
@@ -0,0 +1,159 @@
+$(document).ready(function () {
+  const $hamburger = $(".hamburger");
+  const $asideMenu = $(".aside-container");
+  const $asideMenuLink = $(".aside-container a");
+
+  const $attendeeSlideOne = $(".who-participates-slide-1");
+  const $attendeeSlideTwo = $(".who-participates-slide-2");
+  const $attendeeSlideThree = $(".who-participates-slide-3");
+
+  const $highlightSlideOne = $(".highlight-slider-1");
+  const $highlightSlideTwo = $(".highlight-slider-2");
+  const $highlightSlideThree = $(".highlight-slider-3");
+
+  const $slides = $(
+    ".who-participates-slide-1, .who-participates-slide-2, .who-participates-slide-3"
+  );
+
+  // Toggle Nav
+  const toggleNav = () => {
+    // Toggle hamburger and aside menu
+    $hamburger.on("click", function () {
+      $(this).toggleClass("open");
+      $asideMenu.toggleClass("open");
+    });
+
+    // Close the aside menu when a link is clicked
+    $asideMenuLink.on("click", function () {
+      $asideMenu.removeClass("open");
+      $hamburger.removeClass("open");
+    });
+
+    $(document).on("click", function (event) {
+      if (
+        !$asideMenu.is(event.target) &&
+        !$asideMenu.has(event.target).length &&
+        !$hamburger.is(event.target) &&
+        !$hamburger.has(event.target).length
+      ) {
+        $asideMenu.removeClass("open");
+        $hamburger.removeClass("open");
+      }
+    });
+  };
+
+  const attendeeComp = () => {
+    // Function to calculate slidesToShow based on window width
+    function getSlidesToShow() {
+      return Math.ceil($(window).width() / 250);
+    }
+
+    const slickSettings1 = {
+      infinite: true,
+      speed: 2000,
+      autoplay: true,
+      autoplaySpeed: 10,
+      slidesToShow: getSlidesToShow(),
+      slidesToScroll: 1,
+      lazyLoad: "ondemand",
+      arrows: false,
+      cssEase: "linear",
+      pauseOnHover: false,
+      pauseOnFocus: false,
+      draggable: false,
+    };
+
+    const slickSettings2 = $.extend({}, slickSettings1, {
+      speed: 3000,
+      rtl: true,
+    });
+
+    const slickSettings3 = $.extend({}, slickSettings1, {
+      speed: 6000,
+    });
+
+    $attendeeSlideOne.slick(slickSettings1);
+    $attendeeSlideTwo.slick(slickSettings2);
+    $attendeeSlideThree.slick(slickSettings3);
+
+    // Throttle function to optimize resize event handling
+    let resizeTimeout;
+    $(window).on("resize", function () {
+      clearTimeout(resizeTimeout);
+      resizeTimeout = setTimeout(function () {
+        const newSlidesToShow = getSlidesToShow();
+        $slides.slick("slickSetOption", "slidesToShow", newSlidesToShow, true);
+      }, 200); // Delay to prevent excessive updates during resizing
+    });
+  };
+
+  const highlightComp = () => {
+    const highlightSettings1 = {
+      infinite: true,
+      speed: 4000,
+      autoplay: true,
+      autoplaySpeed: 10,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      lazyLoad: "ondemand",
+      arrows: false,
+      cssEase: "linear",
+
+      responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 567,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    };
+
+    const highlightSettings2 = $.extend({}, highlightSettings1, {
+      speed: 3000,
+      rtl: true,
+    });
+
+    const highlightSettings3 = $.extend({}, highlightSettings1, {
+      speed: 6000,
+    });
+
+    $highlightSlideOne.slick(highlightSettings1);
+    $highlightSlideTwo.slick(highlightSettings2);
+    $highlightSlideThree.slick(highlightSettings3);
+  };
+
+  $(window).scroll(function () {
+    $(".odometer").each(function () {
+      let parent_section_postion = $(this)
+        .closest(".count-container")
+        .position();
+      let parent_section_top = parent_section_postion.top;
+      if (
+        $(window).scrollTop() >
+        parent_section_top - ($(window).height() - 200)
+      ) {
+        if ($(this).data("status") == "yes") {
+          $(this).html($(this).data("count"));
+          $(this).data("status", "no");
+        }
+      }
+    });
+  });
+
+  toggleNav();
+  attendeeComp();
+  highlightComp();
+});
