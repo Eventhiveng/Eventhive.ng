d02c6c80933d4899a332cb249a2f3c524e14b2b8
HaymosBobby <haymosx@gmail.com>
2025-11-05 11:29:36 +0100

    working on 2026 brochure

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
   $(".year").html(year);
 
   // Show modal automatically on page load
-  $("#announcementModal").fadeIn();
+  // $("#announcementModal").fadeIn();
 
   // Hide modal and show main content when clicking close
   $("#closeModal").click(function () {
